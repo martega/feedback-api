8c28dc175d673a5e82fac4ed698f9a8edf331863
Martin Ortega <martin.ortega@mail.rakuten.com>
2013-06-11 11:12:18 -0700

    Adds data access methods for the users entity

--- src/entities/users/users.js
+++ src/entities/users/users.js
@@ -7,9 +7,26 @@ module.exports = Users;
 function Users(db) {
 
   function createUser() {
+    db.collection(app + '.users', function (err, usersCollection) {
+      if (err) {
+        callback(err);
+        return;
+      }
+
+      var user = {
+        answeredQuestions: []
+      };
+
+      feedbackCollection.insert(user, { w: 1 }, function (err, result) {
+        callback(err, result);
+      });
+    });
   }
 
+  //------------------------------------------------------------------------
+
   function getUser(userId) {
+    // TODO
   }
 
   //------------------------------------------------------------------------
